5ebc73ae972861da7d630e97ec6ac9ab73205edd
Ahmed Gaber <ahmed.gaber4371@gmail.com>
2025-12-27 17:19:41 +0200

    Refactor ScanTicketView to use transaction management and improve ticket validation before marking as used

--- tickets/views/scan_ticket.py
+++ tickets/views/scan_ticket.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from rest_framework import status
+from django.db import transaction
 from ..models import Ticket
 from django.shortcuts import get_object_or_404
 from users.permissions import IsStaff
@@ -10,9 +12,12 @@ class ScanTicketView(APIView):
      permission_classes = [IsStaff]
 
      def post(self, request, qr_code, *args, **kwargs):
-          ticket = get_object_or_404(Ticket,qr_code=qr_code)
-          ticket.is_used = True
-          ticket.save()          
+          with transaction.atomic():
+              ticket = Ticket.objects.select_for_update().get(qr_code=qr_code)
+              if ticket.is_used:
+               raise ValidationError("This ticket has already been used")
+              ticket.is_used = True
+              ticket.save()          
           return Response(
                {
                     'message':'the ticket has been scanned successfully'
